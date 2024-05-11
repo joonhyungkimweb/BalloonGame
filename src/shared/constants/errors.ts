bca697f8629ff29875ae28bafcc3eec05b92c08c
joonhyung <asy8010@gmail.com>
2024-05-11 19:44:04 +0900

    [feat] Error presentaional 추가

--- src/shared/constants/errors.ts
+++ src/shared/constants/errors.ts
@@ -1,6 +1,15 @@
-export class InvalidGridSizeError extends Error {
+export class ErrorWithPresentational extends Error {
+  constructor(public message: string, public presentational: string) {
+    super(message);
+  }
+}
+
+export class InvalidGridSizeError extends ErrorWithPresentational {
   name = "InvalidGridSizeError";
   constructor(public size: GridSize) {
-    super("Invalid grid size");
+    super(
+      "Invalid grid size",
+      "그리드 크기가 올바르지 않습니다. 3~10 사이의 숫자를 입력해주세요."
+    );
   }
 }
