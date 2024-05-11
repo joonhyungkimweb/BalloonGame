b2f77c54aaa340833f78000d2bf16804f5313bdd
joonhyung <asy8010@gmail.com>
2024-05-11 22:40:44 +0900

    [feat] searchString 유틸 추가

--- src/shared/utils/stringUtils.ts
+++ src/shared/utils/stringUtils.ts
@@ -1,2 +1,7 @@
 export const parseNumericToNumber = (value: string | undefined): number =>
   Number(value);
+
+export const searchString = (source: string | string[], regex: RegExp) =>
+  Array.isArray(source)
+    ? source.find((str) => regex.test(str))
+    : source.match(regex)?.[0];
