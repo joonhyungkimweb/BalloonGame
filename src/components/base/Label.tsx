25f8b49ac7378f05180c71a6830920bd5acca6bb
joonhyung <asy8010@gmail.com>
2024-05-11 14:49:22 +0900

    [feat] Label Atom props 추가

--- src/components/base/Label.tsx
+++ src/components/base/Label.tsx
@@ -1,26 +1,28 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
-import { HTMLAttributes, PropsWithChildren } from "react";
+import { LabelHTMLAttributes, PropsWithChildren } from "react";
 
 const labelSizes = {
   medium: css`
     font-size: 1rem;
   `,
-  large: css`
-    font-size: 2rem;
-  `,
 };
 
-type LabelProps = PropsWithChildren<HTMLAttributes<HTMLLabelElement>> & {
+type LabelProps = PropsWithChildren<LabelHTMLAttributes<HTMLLabelElement>> & {
   size?: keyof typeof labelSizes;
 };
 
-export default function Label({ children, size = "medium" }: LabelProps) {
+export default function Label({
+  children,
+  size = "medium",
+
+  ...props
+}: LabelProps) {
   return (
     <label
+      {...props}
       css={css`
         ${labelSizes[size]}
-        text-align: center;
       `}
     >
       {children}
